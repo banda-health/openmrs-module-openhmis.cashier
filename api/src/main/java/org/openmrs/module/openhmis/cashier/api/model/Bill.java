5fd7252376457879e68c48b447ba7679f5b1c980
Wes Brown <wes@weslandia.org>
2012-08-27 14:15:40 +0300

    Merged billing changes from Daniel.

--- api/src/main/java/org/openmrs/module/openhmis/cashier/api/model/Bill.java
+++ api/src/main/java/org/openmrs/module/openhmis/cashier/api/model/Bill.java
@@ -1,10 +1,24 @@
-package org.openmrs.module.openhmis.cashier.api.model;
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 
-import java.util.List;
+package org.openmrs.module.openhmis.cashier.api.model;
 
 import org.openmrs.Patient;
 import org.openmrs.Provider;
 import org.openmrs.module.openhmis.cashier.api.IScheme;
+
+import java.util.List;
 
 /**
  * A Bill is a list of {@link BillLineItem}s created by a cashier for a
