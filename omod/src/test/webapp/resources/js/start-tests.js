9b464bb808648f196bb6530da287d042c69e5ccf
Daniel Shorten <danielshorten@gmail.com>
2012-10-18 12:51:09 +0300

    Removed unnecessary code

--- omod/src/test/webapp/resources/js/start-tests.js
+++ omod/src/test/webapp/resources/js/start-tests.js
@@ -1,11 +1,7 @@
 (function() {
-var testPrefix = '/home/daniel/workspace/openhmis/cashier/omod';
-window.OPENHMIS_BASE_URL = '/src/main/webapp/resources/js';
-if (document.URL !== "http://localhost:8234/")
-	OPENHMIS_BASE_URL = testPrefix + OPENHMIS_BASE_URL;
 curl(
 	{
-		baseUrl: OPENHMIS_BASE_URL
+		baseUrl: openhmis.config.wwwUrlRoot + "js"
 	},
 	[
 		'lib/jquery',
@@ -13,9 +9,6 @@ curl(
 	],
 	function($, openhmis) {
 		var testBaseUrl = '/src/test/webapp/resources/js';
-		if (document.URL !== "http://localhost:8234/")
-			testBaseUrl = testPrefix + testBaseUrl;
-		
 		curl(
 			[
 				'js!' + testBaseUrl + '/model/generic.js',
--- omod/src/test/webapp/resources/js/test-bootstrap.js
+++ omod/src/test/webapp/resources/js/test-bootstrap.js
@@ -4,12 +4,13 @@ openhmis.config = {
 	restUrlRoot: "/rest/"
 }
 
+// Global utility function for loading JS modules during a Jasmine spec
 var require = function(map, libs) {
 	libs = libs ? libs : {};
 	for (var lib in libs) { delete libs[lib]; }
 	var list = [];
 	for (var lib in map) { list.push(lib); }
-	curl({ baseUrl: OPENHMIS_BASE_URL}, list, function(something, somethinelse) {
+	curl({ baseUrl: openhmis.config.wwwUrlRoot + "js"}, list, function(something, somethinelse) {
 		libList = {};
 		for (var lib in arguments) {
 			libs[map[list[lib]]] = arguments[lib];
@@ -23,44 +24,3 @@ var require = function(map, libs) {
 		return true;
 	}, "required resources to load", 1000);
 };
-
-//var jQuery, _, Backbone = {}, i18n, openhmis;
-//Backbone.Form = undefined;
-//
-//// htmlunit seems to need this... no clue why
-//openhmis.ItemCode = function() {}
-//
-//// Testing define() that undoes all the good normally done by curl and adds all
-//// libraries to the global namespace, making it easier to run Jasmine test
-//// suites
-//var define = function(name, includes, callback) {
-//	if (name instanceof Array) {
-//		callback = includes;
-//		includes = name;
-//		name = undefined;
-//	}
-//	var includesMap = {
-//		'lib/jquery': jQuery,
-//		'lib/underscore': _,
-//		'lib/backbone': Backbone,
-//		'lib/i18n': i18n,
-//		'openhmis': openhmis,
-//		'lib/backbone-forms': Backbone.Form
-//	}
-//	argv = [];
-//	for (var i in includes)
-//		argv[i] = includesMap[includes[i]] ? includesMap[includes[i]] : openhmis;
-//
-//	// Call JS modules with the libraries they need
-//	var result = callback.apply(window, argv);
-//
-//	// Add certain special cases to window
-//	switch (name) {
-//		case 'lib/backbone':
-//			window.Backbone = result;
-//			break;
-//		case 'lib/i18n':
-//			window.i18n = result;
-//			break;
-//	}
-//}
