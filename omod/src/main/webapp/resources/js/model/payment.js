3b2e4ad22a9d3f3ad60d3e97f8a41b06cddaea4f
dubdabasoduba <dubdabasoduba@gmail.com>
2014-08-28 10:59:22 +0300

    identation

--- omod/src/main/webapp/resources/js/model/payment.js
+++ omod/src/main/webapp/resources/js/model/payment.js
@@ -12,141 +12,141 @@
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
 define(
-    [
-        openhmis.url.backboneBase + 'js/lib/underscore',
-        openhmis.url.backboneBase + 'js/lib/backbone',
-        openhmis.url.backboneBase + 'js/model/generic',
-        openhmis.url.backboneBase + 'js/lib/i18n',
-        openhmis.url.inventoryBase + 'js/model/item',
-        openhmis.url.backboneBase + 'js/model/fieldGenHandler',
-        openhmis.url.backboneBase + 'js/model/openhmis'
-    ],
-    function(_, Backbone, openhmis, __) {
-        openhmis.PaymentAttribute = openhmis.GenericModel.extend({
-            schema: {
-                attributeType: { type: "Object", objRef: true },
-                value: { type: "Text" }
-            },
-            
-            parse: function(resp) {
-                if (resp.attributeType) {
-                    resp.attributeType =
-                        new openhmis.PaymentModeAttributeType(resp.attributeType, { parse: true });
-                }
-                return resp;
-            }
-        });
-        
-        openhmis.Payment = openhmis.GenericModel.extend({
-            meta: {
-                name: "Payment",
-                namePlural: "Payments",
-                restUrl: "v2/cashier/payment"
-            },
-            
-            schema: {
-                dateCreated: { type: 'Text', readOnly: true },
-                dateCreatedFmt: { type: 'Text', title: __("Date"), readOnly: true },
-                amount: { type: 'BasicNumber' },
-                amountFmt: { type: 'BasicNumber', title: __("Amount"), readOnly: true },
-                amountTendered: { type: 'BasicNumber' },
-                amountTenderedFmt: { type: 'BasicNumber', title: __("Amount"), readOnly: true },
-                instanceType: { type: 'Object', objRef: true, title: __("Payment Mode")},
-                attributes: { type: 'List', itemType: 'NestedModel', model: openhmis.PaymentAttribute , title: __("Details") }
-            },
-            
-            url: function() {
-                if (this.meta.parentRestUrl) {
-                    this.urlRoot = this.meta.parentRestUrl +
-                    	this.meta.restUrl.substring(this.meta.restUrl.lastIndexOf('/') + 1);
-                }
-                return openhmis.GenericModel.prototype.url.call(this);
-            },
-            
-   			get: function(attr) {
-				switch (attr) {
-					case 'dateCreatedFmt':
-                        var date = new Date(this.get("dateCreated"));
-						return openhmis.dateFormat(date);
-					case 'amountFmt':
-                        return openhmis.ItemPrice.prototype.format.call(this, this.get("amount"));
-					case 'amountTenderedFmt':
-                        return openhmis.ItemPrice.prototype.format.call(this, this.get("amountTendered"));
-					default:
-						return openhmis.GenericModel.prototype.get.call(this, attr);
+		[
+					openhmis.url.backboneBase + 'js/lib/underscore',
+					openhmis.url.backboneBase + 'js/lib/backbone',
+					openhmis.url.backboneBase + 'js/model/generic',
+					openhmis.url.backboneBase + 'js/lib/i18n',
+					openhmis.url.inventoryBase + 'js/model/item',
+					openhmis.url.backboneBase + 'js/model/fieldGenHandler',
+					openhmis.url.backboneBase + 'js/model/openhmis'
+		],
+		function (_, Backbone, openhmis, __) {
+			openhmis.PaymentAttribute = openhmis.GenericModel.extend({
+				schema: {
+					attributeType: { type: "Object", objRef: true },
+					value: { type: "Text" }
+				},
+
+				parse: function (resp) {
+					if (resp.attributeType) {
+						resp.attributeType =
+								new openhmis.PaymentModeAttributeType(resp.attributeType, { parse: true });
+					}
+					return resp;
 				}
-			},
-            
-            validate: function(goAhead) {
-   				// By default, backbone validates every time we try try to alter
-				// the model.  We don't want to be bothered with this until we
-				// care.
-                if (goAhead !== true) {
-                	return null;
-                }
-                
-                if (this.get("amount") === null || this.get("amount") === undefined) {
-                	return { amount: __("Amount is required.") }
-                }
-                if (isNaN(this.get("amount"))) {
-                    return { amount: __("Amount needs to be a number") }
-                }
-                if (!this.get("instanceType") || !this.get("instanceType").id) {
-                    return { instanceType: __("Payment mode is required.") }
-                }
-                return null;
-            },
-            
-            parse: function(resp) {
-                if (resp.instanceType) {
-                    resp.instanceType = new openhmis.PaymentMode(resp.instanceType);
-                }
-                if (resp.attributes) {
-                    var attributes = resp.attributes;
-                    resp.attributes = [];
-                    for (attr in attributes) {
-                        var paymentAttribute = new openhmis.PaymentAttribute(attributes[attr], { parse: true });
-                        if (attributes[attr].order !== undefined) {
-                            resp.attributes[attributes[attr].order] = paymentAttribute;
-                        } else {
-                            resp.attributes.push(paymentAttribute);
-                        }
-                    }
-                }
-                return resp;
-            },
-            
-            toJSON: function() {
-                var attrs = openhmis.GenericModel.prototype.toJSON.call(this);
-                return attrs;
-            }
-        });
-        
-        openhmis.PaymentModeAttributeType = openhmis.AttributeTypeBase.extend({
-            meta: {
-                restUrl: 'v2/cashier/paymentMode'
-            }
-        });
+			});
 
-        openhmis.PaymentMode = openhmis.CustomizableInstanceTypeBase.extend({
-            attributeType: openhmis.PaymentModeAttributeType,
+			openhmis.Payment = openhmis.GenericModel.extend({
+				meta: {
+					name: "Payment",
+					namePlural: "Payments",
+					restUrl: "v2/cashier/payment"
+				},
 
-            meta: {
-                name: "Payment Mode",
-                namePlural: "Payment Modes",
-                restUrl: 'v2/cashier/paymentMode'
-            },
+				schema: {
+					dateCreated: { type: 'Text', readOnly: true },
+					dateCreatedFmt: { type: 'Text', title: __("Date"), readOnly: true },
+					amount: { type: 'BasicNumber' },
+					amountFmt: { type: 'BasicNumber', title: __("Amount"), readOnly: true },
+					amountTendered: { type: 'BasicNumber' },
+					amountTenderedFmt: { type: 'BasicNumber', title: __("Amount"), readOnly: true },
+					instanceType: { type: 'Object', objRef: true, title: __("Payment Mode")},
+					attributes: { type: 'List', itemType: 'NestedModel', model: openhmis.PaymentAttribute, title: __("Details") }
+				},
 
-            schema: {
-                name: { type: 'Text' },
-                description: { type: 'Text' }
-            },
+				url: function () {
+					if (this.meta.parentRestUrl) {
+						this.urlRoot = this.meta.parentRestUrl +
+								this.meta.restUrl.substring(this.meta.restUrl.lastIndexOf('/') + 1);
+					}
+					return openhmis.GenericModel.prototype.url.call(this);
+				},
 
-            toString: function() {
-                return this.get('name');
-            }
-        });
+				get: function (attr) {
+					switch (attr) {
+						case 'dateCreatedFmt':
+							var date = new Date(this.get("dateCreated"));
+							return openhmis.dateFormat(date);
+						case 'amountFmt':
+							return openhmis.ItemPrice.prototype.format.call(this, this.get("amount"));
+						case 'amountTenderedFmt':
+							return openhmis.ItemPrice.prototype.format.call(this, this.get("amountTendered"));
+						default:
+							return openhmis.GenericModel.prototype.get.call(this, attr);
+					}
+				},
 
-        return openhmis;
-    }
+				validate: function (goAhead) {
+					// By default, backbone validates every time we try try to alter
+					// the model.  We don't want to be bothered with this until we
+					// care.
+					if (goAhead !== true) {
+						return null;
+					}
+
+					if (this.get("amount") === null || this.get("amount") === undefined) {
+						return { amount: __("Amount is required.") }
+					}
+					if (isNaN(this.get("amount"))) {
+						return { amount: __("Amount needs to be a number") }
+					}
+					if (!this.get("instanceType") || !this.get("instanceType").id) {
+						return { instanceType: __("Payment mode is required.") }
+					}
+					return null;
+				},
+
+				parse: function (resp) {
+					if (resp.instanceType) {
+						resp.instanceType = new openhmis.PaymentMode(resp.instanceType);
+					}
+					if (resp.attributes) {
+						var attributes = resp.attributes;
+						resp.attributes = [];
+						for (attr in attributes) {
+							var paymentAttribute = new openhmis.PaymentAttribute(attributes[attr], { parse: true });
+							if (attributes[attr].order !== undefined) {
+								resp.attributes[attributes[attr].order] = paymentAttribute;
+							} else {
+								resp.attributes.push(paymentAttribute);
+							}
+						}
+					}
+					return resp;
+				},
+
+				toJSON: function () {
+					var attrs = openhmis.GenericModel.prototype.toJSON.call(this);
+					return attrs;
+				}
+			});
+
+			openhmis.PaymentModeAttributeType = openhmis.AttributeTypeBase.extend({
+				meta: {
+					restUrl: 'v2/cashier/paymentMode'
+				}
+			});
+
+			openhmis.PaymentMode = openhmis.CustomizableInstanceTypeBase.extend({
+				attributeType: openhmis.PaymentModeAttributeType,
+
+				meta: {
+					name: "Payment Mode",
+					namePlural: "Payment Modes",
+					restUrl: 'v2/cashier/paymentMode'
+				},
+
+				schema: {
+					name: { type: 'Text' },
+					description: { type: 'Text' }
+				},
+
+				toString: function () {
+					return this.get('name');
+				}
+			});
+
+			return openhmis;
+		}
 );
