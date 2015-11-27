7144ee2cf7318181f48b4f87ce808e0452cd008a
Andrew <andrewndungu@gmail.com>
2015-11-27 15:05:14 +0300

    Register openhmis.cashier module using Spring's bean factory

--- api/src/main/java/org/openmrs/module/openhmis/cashier/uiframework/UiConfigurationCashier.java
+++ api/src/main/java/org/openmrs/module/openhmis/cashier/uiframework/UiConfigurationCashier.java
@@ -1,21 +1,40 @@
 package org.openmrs.module.openhmis.cashier.uiframework;
 
-import org.openmrs.annotation.OpenmrsProfile;
-import org.openmrs.ui.framework.StandardModuleUiConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.openmrs.util.OpenmrsClassLoader;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * The OpenMRS UI Framework configuration settings.
  */
-@Configuration
-@OpenmrsProfile(modules = { "uiframework:*.*" })
-public class UiConfigurationCashier {
+public class UiConfigurationCashier implements BeanFactoryPostProcessor {
 
-	@Bean
-	public StandardModuleUiConfiguration createUiConfigurationBean() {
-		StandardModuleUiConfiguration standardModuleUiConfiguration = new StandardModuleUiConfiguration();
-		standardModuleUiConfiguration.setModuleId("openhmis.cashier");
-		return standardModuleUiConfiguration;
+	private static Log log = LogFactory.getLog(UiConfigurationCashier.class);
+
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		log.info("Register openhmis.cashier module");
+		try {
+			// load UiFramework's StandardModuleUiConfiguration class
+			Class cls = OpenmrsClassLoader.getInstance()
+			        .loadClass("org.openmrs.ui.framework.StandardModuleUiConfiguration");
+
+			// get spring's bean definition builder
+			BeanDefinitionBuilder builder = BeanDefinitionBuilder.rootBeanDefinition(cls);
+
+			// set the module Id
+			builder.addPropertyValue("moduleId", "openhmis.cashier");
+
+			// register bean
+			((DefaultListableBeanFactory)beanFactory).registerBeanDefinition(
+			    "openhmisCashierStandardModuleUiConfiguration", builder.getBeanDefinition());
+		} catch (ClassNotFoundException ex) {
+			// StandardModuleUiConfiguration class not found!
+			log.info("Unable to register openhmis.cashier UI 2.x module");
+		}
 	}
 }
