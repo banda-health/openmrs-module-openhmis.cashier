8711f561f56a6729609e2b549ffc23497dfec02e
benjamin mwalimu <b.mwalimu@api-crafttech.com>
2015-12-07 16:26:52 +0300

    having a single instanceId two be used in the different services

--- omod_1.x/src/main/java/org/openmrs/module/webservices/rest/resource/PaymentAttributeResource.java
+++ omod_1.x/src/main/java/org/openmrs/module/webservices/rest/resource/PaymentAttributeResource.java
@@ -15,13 +15,7 @@ package org.openmrs.module.webservices.rest.resource;
 
 import org.apache.log4j.Logger;
 import org.openmrs.Concept;
-import org.openmrs.api.ConceptService;
-import org.openmrs.api.LocationService;
-import org.openmrs.api.ProviderService;
-import org.openmrs.api.UserService;
-import org.openmrs.api.PatientService;
-import org.openmrs.api.EncounterService;
-import org.openmrs.api.ProgramWorkflowService;
+import org.openmrs.api.*;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.openhmis.cashier.api.model.PaymentAttribute;
 import org.openmrs.module.openhmis.cashier.api.model.PaymentModeAttributeType;
@@ -41,88 +35,62 @@ public class PaymentAttributeResource extends BaseRestAttributeDataResource<Paym
 	@Override
 	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
 		DelegatingResourceDescription description = super.getRepresentationDescription(rep);
-		
+
 		if (!(rep instanceof RefRepresentation)) {
 			description.addProperty("valueName", findMethod("getValueName"));
 		}
-		
+
 		return description;
 	}
-	
+
 	@Override
 	public String getDisplayString(PaymentAttribute instance) {
 		String instanceFormat = instance.getAttributeType().getFormat();
 		String names = null;
+		Integer instanceId = null;
+		try {
+			instanceId = Integer.valueOf(instance.getValue());
+		} catch (NumberFormatException ex) {
+			log.error("The instance Id should be a number  " + ex);
+		}
 
-		if (instanceFormat.contains("User")) {
-			UserService userService = Context.getUserService();
-			try {
-				Integer userId = Integer.valueOf(instance.getValue());
-				names = (userService.getUser(userId).getDisplayString());
-			} catch (NumberFormatException ex) {
-				log.error("The user Id should be a number  " + ex);
-			}
-
-		} else if (instanceFormat.contains("Location")) {
-			LocationService locationService = Context.getLocationService();
-			try {
-				Integer locationId = Integer.valueOf(instance.getValue());
-				names= locationService.getLocation(locationId).getDisplayString();
-			} catch (NumberFormatException ex) {
-				log.error("The location Id should be a number " + ex);
-			}
-		} else if (instanceFormat.contains("Provider")) {
-			ProviderService providerService = Context.getProviderService();
-			try {
-				Integer providerId = Integer.valueOf(instance.getValue());
-				names = providerService.getProvider(providerId).getName();
-			} catch (NumberFormatException ex) {
-				log.error("The Provider Id should be a number " + ex);
-			}
-		} else if (instanceFormat.contains("Concept")) {
-			ConceptService conceptService = Context.getConceptService();
-			try {
-				Integer conceptId = Integer.valueOf(instance.getValue());
-				names = conceptService.getConcept(conceptId).getDisplayString();
-			} catch (NumberFormatException ex) {
-				log.error("The Concept Id should be a number " + ex);
-			}
-		} else if (instanceFormat.contains("Patient")) {
-			PatientService patientService = Context.getPatientService();
-			try {
-				Integer patientId = Integer.valueOf(instance.getValue());
-				names = patientService.getPatient(patientId).getPersonName().getFullName();
-			} catch (NumberFormatException ex) {
-				log.error("The Patient Id should be a number " +ex);
-			}
-		} else if (instanceFormat.contains("Encounter")) {
-			EncounterService encounterService = Context.getEncounterService();
-			try {
-				Integer encounterId = Integer.valueOf(instance.getValue());
-				names = encounterService.getEncounter(encounterId).toString();
-			} catch (NumberFormatException ex) {
-				log.error("Encounter Id Should be a number " +ex);
-			}
-		} else if (instanceFormat.contains("ProgramWorkflow")) {
-			ProgramWorkflowService programWorkflowService  = Context.getProgramWorkflowService();
-			try {
-				Integer programWorkflowId = Integer.valueOf(instance.getValue());
-				names = programWorkflowService.getProgram(programWorkflowId).getName();
-			} catch (NumberFormatException ex) {
-				log.error("The Program WorkFlow should be a number " + ex);
+		if (instanceFormat != null) {
+			if (instanceFormat.contains("User")) {
+				UserService userService = Context.getUserService();
+				names = (userService.getUser(instanceId).getDisplayString());
+			} else if (instanceFormat.contains("Location")) {
+				LocationService locationService = Context.getLocationService();
+				names = locationService.getLocation(instanceId).getDisplayString();
+			} else if (instanceFormat.contains("Provider")) {
+				ProviderService providerService = Context.getProviderService();
+				names = providerService.getProvider(instanceId).getName();
+			} else if (instanceFormat.contains("Concept")) {
+				ConceptService conceptService = Context.getConceptService();
+				names = conceptService.getConcept(instanceId).getDisplayString();
+			} else if (instanceFormat.contains("Patient")) {
+				PatientService patientService = Context.getPatientService();
+				names = patientService.getPatient(instanceId).getPersonName().getFullName();
+			} else if (instanceFormat.contains("Encounter")) {
+				EncounterService encounterService = Context.getEncounterService();
+				names = encounterService.getEncounter(instanceId).toString();
+			} else if (instanceFormat.contains("ProgramWorkflow")) {
+				ProgramWorkflowService programWorkflowService = Context.getProgramWorkflowService();
+				names = programWorkflowService.getProgram(instanceId).getName();
+			} else {
+				names = instance.getValue();
 			}
 		} else {
-			names = instance.getValue();
+			log.error("The Instance Format should not be empty");
 		}
 
 		return instance.getAttributeType().getName() + ": " + names;
 	}
-	
+
 	public String getValueName(PaymentAttribute instance) {
 		if (instance.getAttributeType().getFormat().contains("Concept")) {
 			ConceptService service = Context.getService(ConceptService.class);
 			Concept concept = service.getConcept(instance.getValue());
-			
+
 			return concept == null ? "" : concept.getDisplayString();
 		} else {
 			return instance.getValue();
@@ -138,7 +106,7 @@ public class PaymentAttributeResource extends BaseRestAttributeDataResource<Paym
 	public PaymentAttribute newDelegate() {
 		return new PaymentAttribute();
 	}
-	
+
 	@Override
 	public Class<? extends IEntityDataService<PaymentAttribute>> getServiceClass() {
 		return null;
