7048cc64a70bfcd95c6a6a7a2b8f1763346d0528
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2016-08-12 14:17:09 +0300

    Adding the relative cancel page url

--- omod_2.x/src/main/webapp/resources/scripts/cashPoint/controllers/entity.controller.js
+++ omod_2.x/src/main/webapp/resources/scripts/cashPoint/controllers/entity.controller.js
@@ -28,7 +28,7 @@
 
         // @Override
         self.setRequiredInitParameters = self.setRequiredInitParameters || function() {
-                self.bindBaseParameters(module_name, rest_entity_name, entity_name_message_key, CANCEL_PAGE);
+                self.bindBaseParameters(module_name, rest_entity_name, entity_name_message_key, RELATIVE_CANCEL_PAGE_URL);
             };
 
         /**
--- omod_2.x/src/main/webapp/resources/scripts/constants.js
+++ omod_2.x/src/main/webapp/resources/scripts/constants.js
@@ -1,2 +1,3 @@
 var CASHIER_LANDING_PAGE_URL = ROOT_URL + 'openhmis.cashier/cashierLanding.page';
 var MODULE_SETTINGS_URL = 'module/openhmis/cashier/moduleSettings.page';
+var RELATIVE_CANCEL_PAGE_URL = 'entities.page';
