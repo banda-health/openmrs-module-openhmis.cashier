9dfca7ce1b63adafb43c957a8ae0317cb7c7a852
Wes Brown <wes@weslandia.org>
2016-08-17 17:07:25 +0300

    Added Cashier Reports Page.     Created new cashier report page     Added global settings for cashier reports     Added new settings to Cashier Settings pages     ^cash-284 Review

--- omod_1.x/src/main/java/org/openmrs/module/openhmis/cashier/web/controller/JasperReportController.java
+++ omod_1.x/src/main/java/org/openmrs/module/openhmis/cashier/web/controller/JasperReportController.java
@@ -14,7 +14,13 @@
 package org.openmrs.module.openhmis.cashier.web.controller;
 
 import org.apache.commons.lang.StringUtils;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.jasperreport.JasperReport;
+import org.openmrs.module.jasperreport.JasperReportService;
+import org.openmrs.module.jasperreport.ReportParameter;
 import org.openmrs.module.jasperreport.ReportsControllerBase;
+import org.openmrs.module.openhmis.cashier.ModuleSettings;
+import org.openmrs.module.openhmis.cashier.api.model.CashierSettings;
 import org.openmrs.module.openhmis.cashier.web.CashierWebConstants;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +28,10 @@ import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Controller to manage the Jasper Reports page.
@@ -32,13 +41,34 @@ import java.util.HashMap;
 public class JasperReportController extends ReportsControllerBase {
 	@Override
 	public String parse(int reportId, WebRequest request, HttpServletResponse response) throws IOException {
+		CashierSettings settings = ModuleSettings.loadSettings();
+		if (settings.getDefaultShiftReportId() != null && reportId == settings.getDefaultShiftReportId()) {
+			return renderShiftReport(reportId, request, response);
+		} else if (settings.getDepartmentCollectionsReportId() != null
+		        && reportId == settings.getDepartmentCollectionsReportId()) {
+			return renderDateRangeReport(reportId, "Department Collections", request, response);
+		} else if (settings.getDepartmentRevenueReportId() != null
+		        && reportId == settings.getDepartmentRevenueReportId()) {
+			return renderDateRangeReport(reportId, "Department Revenue", request, response);
+		} else if (settings.getDailyShiftSummaryReportId() != null
+		        && reportId == settings.getDailyShiftSummaryReportId()) {
+			return renderDateRangeReport(reportId, "Daily Shift Summary", request, response);
+		} else if (settings.getShiftSummaryReportId() != null
+		        && reportId == settings.getShiftSummaryReportId()) {
+			return renderDateRangeReport(reportId, "Shift Summary", request, response);
+		}
+
+		return null;
+	}
+
+	private String renderShiftReport(int reportId, WebRequest request, HttpServletResponse response) throws IOException {
 		int timesheetId;
 		String temp = request.getParameter("timesheetId");
 		if (!StringUtils.isEmpty(temp) && StringUtils.isNumeric(temp)) {
 			timesheetId = Integer.parseInt(temp);
 		} else {
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "The timesheet id ('" + temp + "') must be "
-			        + "defined and be numeric.");
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "The timesheet id ('" + temp
+			        + "') must be defined and be numeric.");
 			return null;
 		}
 
@@ -46,5 +76,65 @@ public class JasperReportController extends ReportsControllerBase {
 		params.put("timesheetId", timesheetId);
 
 		return renderReport(reportId, params, "Cashier Shift Report - " + temp, response);
+	}
+
+	private String renderDateRangeReport(int reportId, String reportName, WebRequest request,
+	        HttpServletResponse response) throws IOException {
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		if (parseDateRange(reportId, request, response, params)) {
+			String format = request.getParameter("format");
+			if (StringUtils.isEmpty(format)) {
+				format = "pdf";
+			}
+
+			return renderReport(reportId, params, reportName, response, format);
+		} else {
+			return null;
+		}
+	}
+
+	private Boolean parseDateRange(int reportId, WebRequest request, HttpServletResponse response,
+	        Map<String, Object> params) throws IOException {
+		Date beginDate = null, endDate = null;
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		String temp = request.getParameter("beginDate");
+		if (!StringUtils.isEmpty(temp)) {
+			try {
+				beginDate = dateFormat.parse(temp);
+			} catch (Exception ex) {
+				// Whatevs... dealing with stupid checked exceptions
+			}
+		}
+
+		temp = request.getParameter("endDate");
+		if (!StringUtils.isEmpty(temp)) {
+			try {
+				endDate = dateFormat.parse(temp);
+			} catch (Exception ex) {
+				// Whatevs... dealing with stupid checked exceptions
+			}
+		}
+
+		if (beginDate == null || endDate == null) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "The begin and end dates must be defined.");
+
+			return false;
+		}
+
+		// Get the report definition and check to see if the parameter is named beginDate or startDate
+		String beginParameterName = "beginDate";
+		JasperReportService reportService = Context.getService(JasperReportService.class);
+		JasperReport report = reportService.getJasperReport(reportId);
+		for (ReportParameter parameter : report.getParameters()) {
+			if (StringUtils.equalsIgnoreCase(parameter.getName(), "startDate")) {
+				beginParameterName = parameter.getName();
+				break;
+			}
+		}
+
+		params.put(beginParameterName, beginDate);
+		params.put("endDate", endDate);
+
+		return true;
 	}
 }
